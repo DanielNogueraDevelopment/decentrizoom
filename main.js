b22794851504d616b4e8f71b988c29e4ec02dfee
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-16 12:57:55 -0400

    working on it...

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var myurl = { urls: "stun:stun.l.google.com:19302" }
 
 var initiator = new RTCPeerConnection({ iceServers: [myurl] })
-
+var mycam = document.getElementById("me");
+var theircam = document.getElementById("friend");
 
 
 initiator.onaddstream = function(event) {
-    v2.srcObject = event.stream;
+    theircam.srcObject = event.stream;
 }
 
-var
+var UserMedia = navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
+    initiator.addStream(mycam.srcObject = stream)
+})
+
+
+//generate the host key.
+function genhostkey() {
+    UserMedia.then(function() {
+
+
+    })
+}

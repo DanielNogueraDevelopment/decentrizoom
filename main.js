5300a61fbe7fc4cf2c8a52d61f85d3dd6390d2f7
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-16 16:44:12 -0400

    lots of big updates

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var myurl = { urls: "stun:stun.l.google.com:19302" }
 var connection = new RTCPeerConnection({ iceServers: [myurl] })
 var mycam = document.getElementById("me");
 var theircam = document.getElementById("friend");
-var keyinput = document.getElementById("keyinput");
 var keyoutput = document.getElementById("keyoutput");
 connection.onaddstream = function(event) {
     theircam.srcObject = event.stream;
+    theircam.play();
 }
 
-var UserMedia = navigator.getUserMedia({ video: true, audio: false }, function(stream) {
+var UserMedia = navigator.getUserMedia({ video: true, audio: true }, function(stream) {
     mycam.srcObject = stream;
+    mycam.muted = true;
     mycam.play();
     connection.addStream(stream);
 }, function(err) {
@@ -26,17 +27,23 @@ function genhostkey() {
     connection.onicecandidate = function(event) {
 
 
-        var hostkey = LZString.compress(connection.localDescription.sdp);
-        keyoutput.innerHTML = hostkey
+        var hostkey = LZString.compressToUTF16(connection.localDescription.sdp);
+        keyoutput.value = hostkey;
+        document.getElementById("copier").style.display = "inherit";
+        copy()
 
 
 
     }
+    document.getElementById("hostgenerator").innerHTML = "Load Friend Key";
+    document.getElementById("hostgenerator").removeEventListener("click", genhostkey)
+
+
 }
 
 function connecttohost(key) {
-    var hostdesc = new RTCSessionDescription({ type: "offer", sdp: LZString.decompress(key) });
-    console.log(hostdesc)
+    var hostdesc = new RTCSessionDescription({ type: "offer", sdp: LZString.decompressFromUTF16(key) });
+
     connection.setRemoteDescription(hostdesc).then(function() {
         connection.createAnswer();
     }).then(function(mygivendesc) {
@@ -44,12 +51,26 @@ function connecttohost(key) {
     })
 
     connection.onicecandidate = function(event) {
-        return LZString.compress(connection.localDescription.sdp);
+        keyoutput.value = LZString.compressToUTF16(connection.localDescription.sdp);
+        document.getElementById("copier").style.display = "inherit";
+        copy();
     }
 }
 
 function adduser(friendkey) {
+    console.log(LZString.decompressFromUTF16(friendkey));
     if (connection.signalingState == "have-local-offer") {
-        connection.setRemoteDescription(new RTCSessionDescription({ type: "answer", sdp: LZString.decompress(friendkey) }))
+        connection.setRemoteDescription(new RTCSessionDescription({ type: "answer", sdp: LZString.decompressFromUTF16(friendkey) }))
     }
+}
+
+function copy() {
+
+    var copyText = document.getElementById("keyoutput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+
+
 }

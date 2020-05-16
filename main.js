9fa96f2cef3504d1467077eb3602e57eaf16a5ef
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-16 13:33:46 -0400

    user camera is now working

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 var myurl = { urls: "stun:stun.l.google.com:19302" }
 
-var initiator = new RTCPeerConnection({ iceServers: [myurl] })
+var connection = new RTCPeerConnection({ iceServers: [myurl] })
 var mycam = document.getElementById("me");
 var theircam = document.getElementById("friend");
 
 
-initiator.onaddstream = function(event) {
+connection.onaddstream = function(event) {
     theircam.srcObject = event.stream;
 }
 
-var UserMedia = navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
-    initiator.addStream(mycam.srcObject = stream)
-})
+var UserMedia = navigator.getUserMedia({ video: true, audio: false }, function(stream) {
+    mycam.srcObject = stream;
+    mycam.play();
+    connection.addStream(stream);
+}, function(err) {
+    console.log(err)
+});
 
 
 //generate the host key.
 function genhostkey() {
     UserMedia.then(function() {
+        connection.createOffer()
 
-
+    }).then(function(hostdescription) {
+        connection.setLocalDescription(hostdescription)
     })
+
+    connection.onicecandidate = function(event) {
+
+        console.log(connection.localDescription.sdp)
+
+
+
+
+    }
+
+
 }

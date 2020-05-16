be6cb8a39c0de63381eecfd91a407066ae27fef8
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-16 14:57:04 -0400

    more changes

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var myurl = { urls: "stun:stun.l.google.com:19302" }
 var connection = new RTCPeerConnection({ iceServers: [myurl] })
 var mycam = document.getElementById("me");
 var theircam = document.getElementById("friend");
-
+var keyinput = document.getElementById("keyinput");
+var keyoutput = document.getElementById("keyoutput");
 connection.onaddstream = function(event) {
     theircam.srcObject = event.stream;
 }
@@ -26,7 +27,7 @@ function genhostkey() {
 
 
         var hostkey = LZString.compress(connection.localDescription.sdp);
-        console.log(hostkey);
+        keyoutput.innerHTML = hostkey
 
 
 
@@ -35,15 +36,20 @@ function genhostkey() {
 
 function connecttohost(key) {
     var hostdesc = new RTCSessionDescription({ type: "offer", sdp: LZString.decompress(key) });
+    console.log(hostdesc)
     connection.setRemoteDescription(hostdesc).then(function() {
-        connection.createAnswer()
+        connection.createAnswer();
     }).then(function(mygivendesc) {
-        connection.setLocalDescription(mygivendesc)
+        connection.setLocalDescription(mygivendesc);
     })
 
     connection.onicecandidate = function(event) {
-        return LZString(connection.localDescription.sdp)
+        return LZString.compress(connection.localDescription.sdp);
     }
 }
 
-function
+function adduser(friendkey) {
+    if (connection.signalingState == "have-local-offer") {
+        connection.setRemoteDescription(new RTCSessionDescription({ type: "answer", sdp: LZString.decompress(friendkey) }))
+    }
+}

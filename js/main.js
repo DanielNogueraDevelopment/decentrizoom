7068a514c1b822c7d434065e1ab6c5804485b883
danielnogueradevelopment <danielnogueradevelopment@gmail.com>
2020-05-16 22:21:58 -0400

    reasonably stable build

--- js/main.js
+++ js/main.js
@@ -1,16 +1,66 @@
 var myurl = { urls: "stun:stun.l.google.com:19302" }
 
 var connection = new RTCPeerConnection({ iceServers: [myurl] });
-var chatconnection = new RTCPeerConnection({ iceServers: [myurl] })
+var chatconnection = connection.createDataChannel("chat");
+
+chatconnection.onopen = function() {
+    chatadd("Connection established, this is the chat!");
+}
+chatconnection.onmessage = function(event) {
+    console.log("Got a chat message here! ")
+    chatadd(event.data);
+}
+
+
+
+document.getElementById("chatinput").addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+        var message = document.getElementById("chatinput").value;
+        chatconnection.send(message);
+        chatadd(message);
+        document.getElementById("chatinput").value = "";
+    }
+})
+
 var mycam = document.getElementById("me");
 var theircam = document.getElementById("client-feed");
 var keyoutput = document.getElementById("keyoutput");
 const host_feed = document.querySelector('#host-feed');
+var chat = document.getElementById("chat");
 connection.onaddstream = function(event) {
     theircam.srcObject = event.stream;
     theircam.play();
 }
+
+connection.ondatachannel = function(event) {
+    chatconnection = event.channel;
+}
+
+
+
+
 var UserMedia;
+
+function chatadd(text, isme) {
+    if (isme) {
+
+    } else {
+
+    }
+    chat.innerHTML = chat.innerHTML + "<br>" + text;
+    document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+    setTimeout(function() { document.getElementById("chatinput").value = ""; }, 50);
+}
+
+function hidekeytools() {
+    var tools = document.querySelectorAll(".keytool");
+    console.log(tools);
+
+    tools[0].style.display = "none";
+    tools[1].style.display = "none";
+    tools[2].style.display = "none";
+    tools[3].style.display = "none";
+}
 
 
 function loadMedia(iswebcam) {
@@ -31,7 +81,17 @@ function loadMedia(iswebcam) {
             connection.addStream(stream);
         });
     }
-    document.querySelector(".mediaselector").style.display = "none";
+    var selectors = document.querySelectorAll(".mediaselector")
+    selectors[0].style.display = "none";
+    selectors[1].style.display = "none";
+    selectors[2].style.display = "none";
+}
+
+function hideMediaSelectors() {
+    var selectors = document.querySelectorAll(".mediaselector")
+    selectors[0].style.display = "none";
+    selectors[1].style.display = "none";
+    selectors[2].style.display = "none";
 }
 
 
@@ -58,6 +118,7 @@ function genhostkey() {
 }
 
 function connecttohost(key) {
+
     var hostdesc = new RTCSessionDescription({ type: "offer", sdp: LZString.decompressFromUTF16(key) });
 
     connection.setRemoteDescription(hostdesc).then(function() {
@@ -70,14 +131,17 @@ function connecttohost(key) {
         keyoutput.value = LZString.compressToUTF16(connection.localDescription.sdp);
         document.getElementById("keyoutput").style.display = "inherit";
         copy();
+        setTimeout(hidekeytools, 1000);
     }
 }
 
 function adduser(friendkey) {
+
     console.log(LZString.decompressFromUTF16(friendkey));
     if (connection.signalingState == "have-local-offer") {
         connection.setRemoteDescription(new RTCSessionDescription({ type: "answer", sdp: LZString.decompressFromUTF16(friendkey) }))
     }
+    hidekeytools()
 }
 
 function copy() {
@@ -91,31 +155,60 @@ let xOffset = 0,
     init_x,
     init_y,
     current_x,
-    current_y;
+    current_y,
+    lastValidX,
+    lastValidY,
+    active = false;
 
 const dragElementStart = e => {
     e.preventDefault();
-    console.log(e.clientX, e.clientY);
-    current_x = e.clientX - xOffset;
-    current_y = e.clientY - yOffset;
+
+    init_x = e.clientX - xOffset;
+    init_y = e.clientY - yOffset;
+    console.log(current_x, current_y);
+    if (e.target === mycam) active = true;
 }
 
 const dragging = e => {
-    console.log('dragging');
-    e.preventDefault();
-    // if (e.target === mycam) {
-    current_x = e.clientX - init_x;
-    current_y = e.clientY - init_y;
+    if (active) {
+        console.log('dragging');
+        e.preventDefault();
+        const element_style = getComputedStyle(e.target);
+        const width = parseInt(element_style.width.match(/\d+/g)[0]);
+        const height = parseInt(element_style.height.match(/\d+/g)[0]);
 
-    xOffset = current_x;
-    yOffset = current_y;
-    mycam.style.transform = `translate3d(${current_x}px, ${current_y}px, 0)`;
-    // }
+        if (e.target === mycam) {
+            current_x = e.clientX - init_x;
+            current_y = e.clientY - init_y;
+            xOffset = current_x;
+            yOffset = current_y;
+            mycam.style.transform = `translate3d(${current_x}px, ${current_y}px, 0)`;
+        }
+    }
 }
 
-const dragElementEnd = _ => {
+const dragElementEnd = e => {
+    const LEFT_BOUNDS = innerWidth - width;
+    const BOTTOM_BOUNDS = innerHeight - height
+
+    const element_rect = e.target.getBoundingClientRect();
+    const x = element_rect.x;
+    const y = element_rect.y;
+
+    if (x <= LEFT_BOUNDS && x >= 0 && y <= BOTTOM_BOUNDS && y >= 0) {
+        console.log('in bounds');
+        mycam.style.transform = `translate3d(${current_x}px, ${current_y}px, 0)`;
+        lastValidX = current_x;
+        lastValidY = current_y;
+    } else {
+        // mycam.style.transform = `translate3d(${lastValidX}px, ${lastValidY}px, 0)`;
+        mycam.style.left = lastValidX + 'px';
+        mycam.style.top = lastValidX + 'px';
+    }
+
     init_x = current_x;
     init_y = current_y;
+    active = false;
 }
 
 host_feed.addEventListener('mousedown', dragElementStart);
